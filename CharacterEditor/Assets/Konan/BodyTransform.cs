465cf005eac8074a4146028ee8751a9fc1ce29ac
chicchi0531 <coi_mp@yahoo.co.jp>
2017-07-26 12:52:22 +0900

    2017/7/26

---
+++ CharacterEditor/Assets/Konan/BodyTransform.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class MinMax
+{
+    public float Min=0.0f;
+    public float Max=0.0f;
+
+    //Maxに対するMinの比を返す
+    public float Rate() { return Min / Max; }
+}
+
+public class BodyTransform : MonoBehaviour {
+
+    //年齢スライダー
+    [SerializeField]
+    private Slider mSlider = null;
+
+    //スライダーを使わない場合の値
+    [SerializeField, Range(0.0f, 1.0f)]
+    private float mOld = 1.0f;
+
+    //身長の最大値と最小値
+    [SerializeField]
+    private MinMax mMinMaxHeight = null;
+    //身長のカーブ
+    [SerializeField]
+    private AnimationCurve mHeightCurve = null;
+
+    //座高の最大値と最小値
+    [SerializeField]
+    private MinMax mMinMaxSittingHeight = null;
+    //座高のカーブ
+    [SerializeField]
+    private AnimationCurve mSittingHeightCurve = null;
+    
+    //頭身の最大値と最小値(カーブは身長に依存)
+    [SerializeField]
+    private MinMax mMinMaxHeadAndBody = null;
+ 
+    [Space(5)]
+
+    [Header("ボーン設定")]
+
+    //腕　身長の1/4と等しい
+    [SerializeField]
+    private Transform mRightHand = null;
+    [SerializeField]
+    private Transform mLeftHand = null;
+
+    //頭
+    [SerializeField]
+    private Transform mHead = null;
+    //上半身　座高
+    [SerializeField]
+    private Transform mTop = null;
+    //下半身 身長ー座高
+    [SerializeField]
+    private Transform mRightLeg= null;
+    [SerializeField]
+    private Transform mLeftLeg = null;
+
+	// Use this for initialization
+	void Start () {
+        OnChange();
+	}
+	
+	// Update is called once per frame
+	public void OnChange () {
+
+        var value = (mSlider) ? mSlider.value : mOld;
+
+        //身長比
+        var heightRate = mHeightCurve.Evaluate(value) * (1.0f - mMinMaxHeight.Rate()) + mMinMaxHeight.Rate();
+        //座高比
+        var sittingRate = mSittingHeightCurve.Evaluate(value) * (1.0f - mMinMaxSittingHeight.Rate()) + mMinMaxSittingHeight.Rate();
+        //下半身比
+        var bottomRate = 2*heightRate - sittingRate;
+        //頭身比
+        var headRate = (mHeightCurve.Evaluate(value) * (1.0f - mMinMaxHeadAndBody.Rate()) + mMinMaxHeadAndBody.Rate()) * heightRate;
+
+        //値をもとにボーンを更新
+        mRightHand.localScale = new Vector3(heightRate, 1.0f, 1.0f);
+        mLeftHand.localScale = new Vector3(heightRate, 1.0f, 1.0f);
+
+        mHead.localScale = new Vector3(headRate, headRate, headRate);
+
+        //親子関係をいったん外す
+        var parentRH = mRightHand.parent;
+        var parentLH = mLeftHand.parent;
+        var parentHead = mHead.parent;
+        mRightHand.SetParent(null,false);
+        mLeftHand.SetParent(null,false);
+        mHead.SetParent(null,false);
+
+        //親を変形
+        mTop.localScale = new Vector3(sittingRate, (1.0f-sittingRate)*0.5f + sittingRate, (1.0f - sittingRate) * 0.5f + sittingRate);
+
+        //親子関係を戻す
+        mRightHand.SetParent(parentRH,false);
+        mLeftHand.SetParent(parentLH,false);
+        mHead.SetParent(parentHead,false);
+
+        mRightLeg.localScale = new Vector3(bottomRate, bottomRate, bottomRate);
+        mLeftLeg.localScale = new Vector3(bottomRate, bottomRate, bottomRate);
+	}
+}
---
+++ CharacterEditor/Assets/Konan/FaceTransform.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace Konan
+{
+    //頂点の管理クラス
+    public class Point
+    {
+        public int Index { get; set; }
+        public Vector3 Vertex { get; set; }
+    }
+    
+    public class FaceTransform : MonoBehaviour
+    {
+
+        //年齢用スライダー
+        [SerializeField]
+        private Slider mSlider = null;
+
+        //nのカーブ
+        [SerializeField]
+        private AnimationCurve mCurve = null;
+
+        //顔メッシュのインデックス
+        [SerializeField]
+        private int mFaceMeshIndex = 0;
+
+        private SkinnedMeshRenderer[] mDMRenderer = null;
+        private MeshFilter[] mSMFilter = null;
+
+        private Mesh[] mMesh = null;
+
+        private Point[][] mDefaultVertices = null;
+
+        private float mMinPosY = 0.0f;
+        private float mMaxPosY = 0.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            mDMRenderer = GetComponentsInChildren<SkinnedMeshRenderer>();
+            mSMFilter = GetComponentsInChildren<MeshFilter>();
+            mDefaultVertices = new Point[mDMRenderer.Length + mSMFilter.Length][];
+            mMesh = new Mesh[mDMRenderer.Length + mSMFilter.Length];
+
+            //ダイナミックメッシュに対しての処理
+            for (int i = 0; i < mDMRenderer.Length; i++)
+            {
+                //メッシュのクローンをセット
+                mMesh[i] = mDMRenderer[i].sharedMesh;
+                mMesh[i] = Instantiate(mMesh[i]);
+                mMesh[i].MarkDynamic();
+                mDMRenderer[i].sharedMesh = mMesh[i];
+
+                //サブメッシュのインデックスリストを取得
+                var triangles = mMesh[i].GetTriangles(mFaceMeshIndex);
+                var trianglesUnique = triangles.Distinct().ToArray();   //重複を削除
+
+                //デフォルトの頂点を保存
+                mDefaultVertices[i] = trianglesUnique.Select(x => new Point { Index = x, Vertex = mMesh[i].vertices[x] }).ToArray();
+            }
+
+            //スタティックメッシュに対しての処理
+            for(int i=mDMRenderer.Length;i<mSMFilter.Length+mDMRenderer.Length;i++)
+            {
+                mMesh[i] = mSMFilter[i - mDMRenderer.Length].mesh;
+                mMesh[i].MarkDynamic();
+                //mSMFilter[i-mDMRenderer.Length].mesh = mMesh[i];
+
+                //サブメッシュのインデックスリストを取得
+                var triangles = mMesh[i].GetTriangles(mFaceMeshIndex);
+                var trianglesUnique = triangles.Distinct().ToArray();   //重複を削除
+
+                //デフォルトの頂点を保存
+                mDefaultVertices[i] = trianglesUnique.Select(x => new Point { Index = x, Vertex = mMesh[i].vertices[x] }).ToArray();
+            }
+
+            //頂点のY軸方向の最小値と最大値を保存
+            mMinPosY = mDefaultVertices.Select(x => x.Select(y => y.Vertex.y).Aggregate((n, next) => Mathf.Min(n, next))).Aggregate((m, next) => Mathf.Min(m, next));
+            mMaxPosY = mDefaultVertices.Select(x => x.Select(y => y.Vertex.y).Aggregate((n, next) => Mathf.Max(n, next))).Aggregate((m, next) => Mathf.Max(m, next));
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnChange()
+        {
+            //スライダーの値を取得
+            var n = mCurve.Evaluate(mSlider.value)+1.0f;
+
+
+            for (int i = 0; i < mDMRenderer.Length + mSMFilter.Length; i++)
+            {
+                //現在の頂点
+                var vertices = mMesh[i].vertices;
+
+                //新しい頂点座標を計算
+                foreach (var point in mDefaultVertices[i])
+                {
+                    var y = point.Vertex.y;
+                    var mappedY = 0.0f;
+
+                    try
+                    {
+                        //y座標を0-1空間にマッピング
+                        mappedY = ((y - mMinPosY) / (mMaxPosY - mMinPosY));
+                    }
+                    catch (DivideByZeroException e)
+                    {
+                        Debug.Log("0除算が行われました。Y軸方向に厚みのあるオブジェクトを使用してください");
+                    }
+
+                    //b=a^nに従って座標を変換
+                    mappedY = Mathf.Pow(mappedY, n);
+
+                    //元の座標空間に戻す
+                    y = mappedY * (mMaxPosY - mMinPosY) + mMinPosY;
+
+                    //新しい頂点を構成
+                    var newVertex = point.Vertex;
+                    newVertex.y = y;
+
+                    vertices[point.Index] = newVertex;
+                }
+                
+
+                mMesh[i].vertices = vertices;
+                mMesh[i].RecalculateNormals(); //法線再計算
+                mMesh[i].RecalculateBounds();  //BoundingVolume再計算
+            }
+        }
+    }
+}
