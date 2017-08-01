3f6ef473eb8e0a36969150f52783edfa163b4756
chicchi0531 <coi_mp@yahoo.co.jp>
2017-08-01 14:00:22 +0900

    2017/8/1

--- CharacterEditor/Assets/Konan/BodyTransform.cs
+++ CharacterEditor/Assets/Konan/BodyTransform.cs
@@ -4,108 +4,140 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
-[Serializable]
-public class MinMax
+namespace Konan
 {
-    public float Min=0.0f;
-    public float Max=0.0f;
 
-    //Maxに対するMinの比を返す
-    public float Rate() { return Min / Max; }
-}
+    [Serializable]
+    public class MinMax
+    {
+        public float Min = 0.0f;
+        public float Max = 0.0f;
 
-public class BodyTransform : MonoBehaviour {
+        //Maxに対するMinの比を返す
+        public float Rate() { return Min / Max; }
+    }
 
-    //年齢スライダー
-    [SerializeField]
-    private Slider mSlider = null;
+    public class BodyTransform : MonoBehaviour {
 
-    //スライダーを使わない場合の値
-    [SerializeField, Range(0.0f, 1.0f)]
-    private float mOld = 1.0f;
+        //年齢スライダー
+        [SerializeField]
+        private Slider mSlider = null;
 
-    //身長の最大値と最小値
-    [SerializeField]
-    private MinMax mMinMaxHeight = null;
-    //身長のカーブ
-    [SerializeField]
-    private AnimationCurve mHeightCurve = null;
+        //スライダーを使わない場合の値
+        [SerializeField, Range(0.0f, 1.0f)]
+        private float mOld = 1.0f;
 
-    //座高の最大値と最小値
-    [SerializeField]
-    private MinMax mMinMaxSittingHeight = null;
-    //座高のカーブ
-    [SerializeField]
-    private AnimationCurve mSittingHeightCurve = null;
-    
-    //頭身の最大値と最小値(カーブは身長に依存)
-    [SerializeField]
-    private MinMax mMinMaxHeadAndBody = null;
- 
-    [Space(5)]
+        //身長の最大値と最小値
+        [SerializeField]
+        private MinMax mMinMaxHeight = null;
+        //身長のカーブ
+        [SerializeField]
+        private AnimationCurve mHeightCurve = null;
 
-    [Header("ボーン設定")]
+        //座高の最大値と最小値
+        [SerializeField]
+        private MinMax mMinMaxSittingHeight = null;
+        //座高のカーブ
+        [SerializeField]
+        private AnimationCurve mSittingHeightCurve = null;
 
-    //腕　身長の1/4と等しい
-    [SerializeField]
-    private Transform mRightHand = null;
-    [SerializeField]
-    private Transform mLeftHand = null;
+        //頭身の最大値と最小値(カーブは身長に依存)
+        [SerializeField]
+        private MinMax mMinMaxHeadAndBody = null;
 
-    //頭
-    [SerializeField]
-    private Transform mHead = null;
-    //上半身　座高
-    [SerializeField]
-    private Transform mTop = null;
-    //下半身 身長ー座高
-    [SerializeField]
-    private Transform mRightLeg= null;
-    [SerializeField]
-    private Transform mLeftLeg = null;
+        [Space(5)]
+        [Header("身体特徴の設定")]
+        //上半身の太さ変化率
+        [SerializeField, Range(0.0f, 1.0f)]
+        private float mTopThicknessRate = 0.5f;
 
-	// Use this for initialization
-	void Start () {
-        OnChange();
-	}
-	
-	// Update is called once per frame
-	public void OnChange () {
+        //足の太さ変化率
+        [SerializeField,Range(0.0f,1.0f)]
+        private float mLegThicknessRate = 0.0f;
 
-        var value = (mSlider) ? mSlider.value : mOld;
+        [Space(5)]
 
-        //身長比
-        var heightRate = mHeightCurve.Evaluate(value) * (1.0f - mMinMaxHeight.Rate()) + mMinMaxHeight.Rate();
-        //座高比
-        var sittingRate = mSittingHeightCurve.Evaluate(value) * (1.0f - mMinMaxSittingHeight.Rate()) + mMinMaxSittingHeight.Rate();
-        //下半身比
-        var bottomRate = 2*heightRate - sittingRate;
-        //頭身比
-        var headRate = (mHeightCurve.Evaluate(value) * (1.0f - mMinMaxHeadAndBody.Rate()) + mMinMaxHeadAndBody.Rate()) * heightRate;
+        [Header("ボーン設定")]
 
-        //値をもとにボーンを更新
-        mRightHand.localScale = new Vector3(heightRate, 1.0f, 1.0f);
-        mLeftHand.localScale = new Vector3(heightRate, 1.0f, 1.0f);
+        //腕　身長の1/4と等しい
+        [SerializeField]
+        private Transform mRightHand = null;
+        [SerializeField]
+        private Transform mLeftHand = null;
 
-        mHead.localScale = new Vector3(headRate, headRate, headRate);
+        //頭
+        [SerializeField]
+        private Transform mHead = null;
+        //上半身　座高
+        [SerializeField]
+        private Transform mTop = null;
+        //下半身 身長ー座高
+        [SerializeField]
+        private Transform mRightLeg = null;
+        [SerializeField]
+        private Transform mLeftLeg = null;
 
-        //親子関係をいったん外す
-        var parentRH = mRightHand.parent;
-        var parentLH = mLeftHand.parent;
-        var parentHead = mHead.parent;
-        mRightHand.SetParent(null,false);
-        mLeftHand.SetParent(null,false);
-        mHead.SetParent(null,false);
+        //内部変数
+        private FreeTransform[] mFreeTransforms = null;
 
-        //親を変形
-        mTop.localScale = new Vector3(sittingRate, (1.0f-sittingRate)*0.5f + sittingRate, (1.0f - sittingRate) * 0.5f + sittingRate);
+        // Use this for initialization
+        void Start() {
+            //自由変形コンポーネントを全取得
+            mFreeTransforms = GetComponentsInChildren<FreeTransform>();
 
-        //親子関係を戻す
-        mRightHand.SetParent(parentRH,false);
-        mLeftHand.SetParent(parentLH,false);
-        mHead.SetParent(parentHead,false);
+            OnChange();
+        }
 
-        mRightLeg.localScale = new Vector3(bottomRate, bottomRate, bottomRate);
-        mLeftLeg.localScale = new Vector3(bottomRate, bottomRate, bottomRate);
-	}
+        // Update is called once per frame
+        public void OnChange() {
+
+            var value = (mSlider) ? mSlider.value : mOld;
+
+            //身長比
+            var heightRate = mHeightCurve.Evaluate(value) * (1.0f - mMinMaxHeight.Rate()) + mMinMaxHeight.Rate();
+            //座高比
+            var sittingRate = mSittingHeightCurve.Evaluate(value) * (1.0f - mMinMaxSittingHeight.Rate()) + mMinMaxSittingHeight.Rate();
+            //下半身比
+            var bottomRate = 2 * heightRate - sittingRate;
+            //頭身比
+            var headRate = (mHeightCurve.Evaluate(value) * (1.0f - mMinMaxHeadAndBody.Rate()) + mMinMaxHeadAndBody.Rate()) * heightRate;
+
+            //値をもとにボーンを更新
+            mRightHand.localScale = new Vector3(heightRate, 1.0f, 1.0f);
+            mLeftHand.localScale = new Vector3(heightRate, 1.0f, 1.0f);
+
+            mHead.localScale = new Vector3(headRate, headRate, headRate);
+
+            //親子関係をいったん外す
+            var parentRH = mRightHand.parent;
+            var parentLH = mLeftHand.parent;
+            var parentHead = mHead.parent;
+            mRightHand.SetParent(null, false);
+            mLeftHand.SetParent(null, false);
+            mHead.SetParent(null, false);
+
+            //親を変形
+            mTop.localScale = new Vector3(sittingRate, 
+                (1.0f - sittingRate) * mTopThicknessRate + sittingRate, 
+                (1.0f - sittingRate) * mTopThicknessRate + sittingRate);
+
+            //親子関係を戻す
+            mRightHand.SetParent(parentRH, false);
+            mLeftHand.SetParent(parentLH, false);
+            mHead.SetParent(parentHead, false);
+
+            mRightLeg.localScale = new Vector3(bottomRate, 
+                (1.0f - bottomRate) * mLegThicknessRate + bottomRate, 
+                (1.0f - bottomRate) * mLegThicknessRate + bottomRate);
+            mLeftLeg.localScale = new Vector3(bottomRate,
+                (1.0f - bottomRate) * mLegThicknessRate + bottomRate,
+                (1.0f - bottomRate) * mLegThicknessRate + bottomRate);
+
+            //変形後に自由変形ノードを変形させる
+            foreach (var f in mFreeTransforms)
+            {
+                f.OnChange(heightRate);
+            }
+        }
+    }
 }
---
+++ CharacterEditor/Assets/Konan/FreeTransform.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Konan
+{
+    public enum Axis
+    {
+        X, Y, Z
+    }
+
+    public class FreeTransform : MonoBehaviour
+    {
+        //親の変形ボーン
+        [SerializeField]
+        private Transform mParent = null;
+
+        //長さ方向の軸
+        [SerializeField]
+        private Axis mLenghAxis = Axis.X;
+
+        //ボーンスケールの変化率、太さ
+        [SerializeField, Range(-3.0f, 1.0f)]
+        float mThickness = 0.0f;
+
+        //ボーンスケールの変化率、長さ
+        [SerializeField, Range(-3.0f, 1.0f)]
+        float mLength = 0.0f;
+
+        public void OnChange(float eval)
+        {
+            //現在のスケールを取得
+            var scale = mParent.localScale;
+            var scale_thick1 = 0.0f;
+            var scale_thick2 = 0.0f;
+            var scale_length = 0.0f;
+
+            //軸の選択
+            switch (mLenghAxis)
+            {
+                case Axis.X:
+                    scale_thick1 = scale.y; scale_thick2 = scale.z; scale_length = scale.x;
+                    break;
+                case Axis.Y:
+                    scale_thick1 = scale.x; scale_thick2 = scale.z; scale_length = scale.y;
+                    break;
+                case Axis.Z:
+                    scale_thick1 = scale.x; scale_thick2 = scale.y; scale_length = scale.z;
+                    break;
+            }
+
+            //変化率の計算
+            var thick = mThickness * eval;
+            var length = mLength * eval;
+
+            //スケールを変化率に応じて変更する
+            scale_thick1 = 1.0f - ((1.0f - scale_thick1) * thick);
+            scale_thick2 = 1.0f - ((1.0f - scale_thick2) * thick);
+            scale_length = 1.0f - ((1.0f - scale_length) * length);
+
+            //値の代入
+            switch (mLenghAxis)
+            {
+                case Axis.X:
+                    transform.localScale = new Vector3(1.0f / scale_length, 1.0f / scale_thick1, 1.0f / scale_thick2); break;
+                case Axis.Y:
+                    transform.localScale = new Vector3(1.0f / scale_thick1, 1.0f / scale_length, 1.0f / scale_thick2); break;
+                case Axis.Z:
+                    transform.localScale = new Vector3(1.0f / scale_thick1, 1.0f / scale_thick2, 1.0f / scale_length); break;
+
+            }
+        }
+    }
+}
